23d832bc19af58bb469f5a47bf88bd2e375f84ca
florent <florent@florent-desktop.(none)>
2010-11-20 19:06:06 +0100

    update controller/app import

--- bewype/flask/controllers/__init__.py
+++ bewype/flask/controllers/__init__.py
@@ -3,3 +3,9 @@
 
 __import__('pkg_resources').declare_namespace(__name__)
 
+# bewype import
+from bewype.flask import app
+
+import bewype.flask.controllers.info
+import bewype.flask.controllers.upload
+
